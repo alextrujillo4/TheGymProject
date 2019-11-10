d3c759398a6b03ac6752184d03df0449c4c8816b
Alex Trujillo <a01328426@itesm.mx>
2019-11-10 15:39:54 -0600

    conexion exitosa con firebase functions. Cors agregado

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({origin: true});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
- });
+exports.helloWorld = functions.https.onRequest( (request, response) => {
+    cors(request, response, () => {
+        /*if (!request.body.queryResult) {
+        console.log('Invalid Request');
+        return response.status(400).end('Invalid Request.');
+    } else {*/
+        processRequest(request, response);
+        //}
+    });
+});
 
+function processRequest(request, response){
+    response.status(200).send({test: 'Conexion Exitosa'});
+
+}
