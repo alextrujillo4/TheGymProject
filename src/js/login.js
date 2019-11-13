44a71c09fe99d9542d14d809eda026fbb1c9d352
Alex Trujillo <a01328426@itesm.mx>
2019-11-13 00:00:00 -0600

    Changes added to Project

--- src/js/login.js
+++ src/js/login.js
@@ -1,7 +1,5 @@
-import "../home.html"
-
-import {MDCTextField} from '@material/textfield';
 import {MDCRipple} from '@material/ripple';
+import {MDCTextField} from '@material/textfield';
 
 const username = new MDCTextField(document.querySelector('.username'));
 const password = new MDCTextField(document.querySelector('.password'));
---
+++ src/webpack.prod.config.js
@@ -0,0 +1,98 @@
+const path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+function getStyleUse(bundleFilename) {
+  return [
+    {
+      loader: 'file-loader',
+      options: {
+        name: bundleFilename,
+      },
+    },
+    { loader: 'extract-loader' },
+    { loader: 'css-loader' },
+    {
+      loader: 'sass-loader',
+      options: {
+        includePaths: ['./node_modules'],
+        implementation: require('dart-sass'),
+        fiber: require('fibers'),
+      }
+    },
+  ];
+}
+
+module.exports = [
+  {
+    entry: './scss/login.scss',
+    output: {
+      path: path.resolve(__dirname, "../public/css/"),
+      // This is necessary for webpack to compile, but we never reference this js file.
+      filename: 'style-bundle-login.js',
+    },
+    module: {
+      rules: [{
+        test: /login.scss$/,
+        use: getStyleUse('css/bundle-login.css')
+      }]
+    },
+  },
+  {
+    entry: './scss/home.scss',
+    output: {
+      path: path.resolve(__dirname, "../public/css/"),
+      // This is necessary for webpack to compile, but we never reference this js file.
+      filename: 'style-bundle-home.js',
+    },
+    module: {
+      rules: [{
+        test: /home.scss$/,
+        use: getStyleUse('css/bundle-home.css')
+      }]
+    },
+  },
+
+  //================= JS FILES AND HTML  ====================
+  {
+    entry: "./js/login.js",
+    output: {
+      path: path.resolve(__dirname, "../public/"),
+      filename: "bundle-login.js"
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        hash: true,
+        template: 'index.html',
+        filename: 'index.html' //relative to root of the application
+      })
+    ],
+    module: {
+      loaders: [{
+        test: /login.js$/,
+        loader: 'babel-loader',
+        query: {presets: ['env']}
+      }]
+    },
+  },
+  {
+    entry: "./js/home.js",
+    output: {
+      path: path.resolve(__dirname, "../public/"),
+      filename: "bundle-home.js"
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        hash: true,
+        template: 'home.html',
+        filename: 'home.html' //relative to root of the application
+      })
+    ],
+    module: {
+      loaders: [{
+        test: /home.js$/,
+        loader: 'babel-loader',
+        query: {presets: ['env']}
+      }]
+    },
+  }
+];
