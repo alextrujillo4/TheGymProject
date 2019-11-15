3dc93fafccfd7ebca265d2049fae9e53fbab739a
Alex Trujillo <a01328426@itesm.mx>
2019-11-14 18:01:52 -0600

    create user in firebase function added

--- src/js/register.js
+++ src/js/register.js
@@ -1,7 +1,14 @@
 import {MDCTextField} from '@material/textfield';
+
 import {MDCRipple} from '@material/ripple';
-var admin = require("firebase-admin");
-admin.initializeApp({
+
+const URL = " https://us-central1-gymproject-9f46b.cloudfunctions.net";
+const username = new MDCTextField(document.querySelector('.username'));
+const email = new MDCTextField(document.querySelector('.email'));
+const password = new MDCTextField(document.querySelector('.password'));
+const confirmPassword = new MDCTextField(document.querySelector('.confirmPassword'));
+
+const firebaseConfig = {
     apiKey: "AIzaSyCMQfCVoDb2eiuXHACCUX66TO_6v4XFTF0",
     authDomain: "gymproject-9f46b.firebaseapp.com",
     databaseURL: "https://gymproject-9f46b.firebaseio.com",
@@ -10,29 +17,37 @@ admin.initializeApp({
     messagingSenderId: "585009595190",
     appId: "1:585009595190:web:cad18d95185a486bf1e997",
     measurementId: "G-TSWB8RLHM9"
-});
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const auth  = firebase.auth();
 
-const URL = " https://us-central1-gymproject-9f46b.cloudfunctions.net";
-const username = new MDCTextField(document.querySelector('.username'));
-const mail = new MDCTextField(document.querySelector('.email'));
-const password = new MDCTextField(document.querySelector('.password'));
-const confirmPassword = new MDCTextField(document.querySelector('.confirmPassword'));
-const registerbutton = new MDCRipple(document.querySelector('#next'));
 
 function registerAction() {
     console.log("registerAction()");
     let registerBtn = document.getElementById('next');
     registerBtn.addEventListener('click', function(event) {
         event.preventDefault();
-      if(username.valid && mail.valid && password.valid && confirmPassword.valid){
+      if(username.valid && email.valid && password.valid && confirmPassword.valid){
           registerUser();
       }
     });
 
 }
 
-/*function registerUser() {
-    $.ajax({
+function registerUser() {
+    auth.createUserWithEmailAndPassword(email.value, password.value)
+        .then(function(firebaseUser) {
+            console.log("User " + firebaseUser.uid + " created successfully!");
+        })
+        .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+    });
+    /*$.ajax({
         url: URL + "/register",
         method: "POST",
         dataType: "json",
@@ -50,6 +65,6 @@ function registerAction() {
             console.log("Connexión Error");
             console.log(err)
         }
-    });
-}*/
+    });*/
+}
 registerAction();
