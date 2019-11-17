3aaf3749b0d52833c909b575d3602d66ba2ca99c
Alex Trujillo <a01328426@itesm.mx>
2019-11-17 14:27:30 -0600

    Registro de usuarios. Coneción a Firestore and functions

--- src/js/register.js
+++ src/js/register.js
@@ -1,13 +1,18 @@
 import {MDCTextField} from '@material/textfield';
-
+import {MDCDialog} from '@material/dialog';
+const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
 import {MDCRipple} from '@material/ripple';
+import { MDCLinearProgress } from '@material/linear-progress';
 
-const URL = " https://us-central1-gymproject-9f46b.cloudfunctions.net";
+//const URL = "https://us-central1-gymproject-9f46b.cloudfunctions.net";
+const URL = "http://localhost:5000/gymproject-9f46b/us-central1"
+
 const username = new MDCTextField(document.querySelector('.username'));
 const email = new MDCTextField(document.querySelector('.email'));
 const password = new MDCTextField(document.querySelector('.password'));
 const confirmPassword = new MDCTextField(document.querySelector('.confirmPassword'));
-
+const linearProgress = new MDCLinearProgress(document.querySelector('.mdc-linear-progress'));
+linearProgress.close();
 const firebaseConfig = {
     apiKey: "AIzaSyCMQfCVoDb2eiuXHACCUX66TO_6v4XFTF0",
     authDomain: "gymproject-9f46b.firebaseapp.com",
@@ -18,11 +23,18 @@ const firebaseConfig = {
     appId: "1:585009595190:web:cad18d95185a486bf1e997",
     measurementId: "G-TSWB8RLHM9"
 };
-// Initialize Firebase
+
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 const auth  = firebase.auth();
 
+function dialogAction(){
+    console.log("dialogAction()");
+    let okBtn = document.getElementById('dialog_ok');
+    okBtn.addEventListener('click', function(event) {
+        window.location.href='/'
+    });
+}
 
 function registerAction() {
     console.log("registerAction()");
@@ -30,41 +42,54 @@ function registerAction() {
     registerBtn.addEventListener('click', function(event) {
         event.preventDefault();
       if(username.valid && email.valid && password.valid && confirmPassword.valid){
-          registerUser();
+         registerUser();
       }
     });
-
 }
 
-function registerUser() {
-    auth.createUserWithEmailAndPassword(email.value, password.value)
-        .then(function(firebaseUser) {
-            console.log("User " + firebaseUser.uid + " created successfully!");
-        })
-        .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-    });
-    /*$.ajax({
+
+function registerUserInDatabase(uid) {
+    $.ajax({
         url: URL + "/register",
         method: "POST",
         dataType: "json",
         data:{
-            username : username.value,
-            mail : mail.value,
-            password : password.value
+            user : {
+                uid:uid,
+                username : username.value,
+                mail : email.value,
+            }
         },
         success: responseJSON => {
-            console.log("Connexión Exitosa");
-            console.log(responseJSON)
-
+            console.log("Conexión Exitosa");
+            console.log(responseJSON.status);
+            if (responseJSON.status === 200){
+                dialog.open()
+            } else{
+                alert("Error... user is already registered!")
+            }
         },
         error: function(err) {
-            console.log("Connexión Error");
-            console.log(err)
+
+            console.log("User Not registered");
+            alert("Error... user Not registered :(")
         }
-    });*/
+    });
+    linearProgress.close();
 }
+function registerUser() {
+    linearProgress.open();
+    linearProgress.determinate = false;
+    auth.createUserWithEmailAndPassword(email.value, password.value)
+        .then(function(firebaseUser) {
+            console.log("User: " + firebaseUser.user.uid+ "Created successfully!");
+            registerUserInDatabase(firebaseUser.user.uid);
+        })
+        .catch(function(error) {
+            console.log("User not Created!");
+            linearProgress.close();
+        });
+}
+
 registerAction();
+dialogAction();
