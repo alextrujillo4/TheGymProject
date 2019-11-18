26a496cae807fb6d8a8e691d62512284327a35be
Alex Trujillo <a01328426@itesm.mx>
2019-11-18 02:28:07 -0600

    commit of list

--- src/js/home.js
+++ src/js/home.js
@@ -1,12 +1,13 @@
-
-
 import {auth, firestore} from "./firebase";
-
 import {MDCList} from '@material/list';
 import {MDCRipple} from '@material/ripple';
 import {MDCDialog} from '@material/dialog';
+import {MDCIconButtonToggle} from '@material/icon-button';
+
 const dialog = new MDCDialog(document.getElementById('mdc-logout-dialog'));
 const fabRipple = new MDCRipple(document.querySelector('.mdc-fab'));
+const iconToggle = new MDCIconButtonToggle(document.querySelector('.mdc-icon-button'));
+
 new MDCList(document.querySelector('.mdc-list'));
 window.onload = function verifyUser() {
     auth.onAuthStateChanged(function (user) {
@@ -19,25 +20,33 @@ window.onload = function verifyUser() {
 $("#createbtn").on("click", function (event) {
 event.preventDefault();
     $("#cardRoutine").append(`
-        <div class="mdc-layout-grid__cell">
-            <div class="mdc-card routine">
-            <div class="mdc-card__primary-action">
-            <div class="mdc-card__media mdc-card__media--square">
-            <div class="mdc-card__media-content">Title</div>
-            </div>
-            </div>
-            <div class="mdc-card__actions">
-                <div class="mdc-card__action-buttons">
-                 <button class="mdc-button mdc-card__action mdc-card__action--button">
-                    <span class="mdc-button__label">Ver</span>
-                </button>
-                <button class="mdc-button mdc-card__action mdc-card__action--button">
-                    <span class="mdc-button__label">Editar</span>
-                </button>
+        <div class="mdc-card routine">
+                    <div class="mdc-card__primary-action demo-card__primary-action my-card-content" tabindex="0">
+                        <div class="demo-card__primary">
+                            <h2 class="demo-card__title mdc-typography mdc-typography--headline6">Day 0</h2>
+                            <h3 class="demo-card__subtitle mdc-typography mdc-typography--subtitle2">by Alex Trujillo</h3>
+                        </div>
+                        <div class="demo-card__secondary mdc-typography mdc-typography--body2">This is the first routine. Visit ten places on our planet that are undergoing the biggest changes today.</div>
+                    </div>
+                    <div class="mdc-card__actions">
+                        <div class="mdc-card__action-buttons">
+                            <button class="mdc-button mdc-card__action mdc-card__action--button">  <span class="mdc-button__ripple"></span> Read</button>
+                            <button class="mdc-button mdc-card__action mdc-card__action--button">  <span class="mdc-button__ripple"></span> Edit</button>
+                        </div>
+                        <div class="mdc-card__action-icons">
+                            <button id="add-to-pubic"
+                                    class="mdc-icon-button"
+                                    aria-label="Add to public"
+                                    aria-hidden="true"
+                                    aria-pressed="false">
+                                <i class="material-icons mdc-icon-button__icon mdc-icon-button__icon--on">visibility</i>
+                                <i class="material-icons mdc-icon-button__icon">visibility_off</i>
+                            </button>
+                            <button class="material-icons mdc-icon-button mdc-card__action mdc-card__action--icon" title="Delete">delete</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            </div>
-        </div>`);
+`);
 });
 
 
